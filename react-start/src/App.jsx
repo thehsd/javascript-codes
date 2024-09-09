731d2e4a62bf7c538eb87bae220c94d4f4619884
thehsd <hosseinialireza485@gmail.com>
2024-09-09 18:12:20 +0330

    react 3

--- react-start/src/App.jsx
+++ react-start/src/App.jsx
@@ -1,10 +1,27 @@
-import { Fragment } from "react";
-import FirstComponent from "./components/FirstComponent";
+import React, { useEffect, useState } from "react";
+import Button from "./components/common/button";
+function App() {
+  const [postList, setPostList] = useState([]);
 
-export const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiFetch = await fetch(
+        "https://jsonplaceholder.typicode.com/posts/"
+      );
+      const data = await apiFetch.json();
+      setPostList(data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <FirstComponent />
-    </>
+    <div>
+      <Button variant={"primary"}>click</Button>
+      {postList.map((post) => {
+        return <li>{post.title}</li>;
+      })}
+    </div>
   );
-};
+}
+
+export default App;
